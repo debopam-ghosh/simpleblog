bf0a2ae1a436a47055d7ddf1a26de5d1a8c11947
debopam-ghosh <debopamghosh99@gmail.com>
2025-03-26 15:45:43 +0530

    K

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ from wtforms import StringField, PasswordField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-app.config['SECRET_KEY'] = 'NuH9XSwRp4'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
@@ -112,7 +113,7 @@ def post():
 with app.app_context():
     db.create_all()
 
-import os
+
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 80))  
     app.run(host='0.0.0.0', port=port)
